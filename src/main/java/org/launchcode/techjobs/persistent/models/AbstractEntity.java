c3a436de3089f9f3b97eb80e30fb86181c058cac
Hal's Laptop <halrobisoncreative@gmail.com>
2024-06-08 10:37:27 -0500

    task 2 - updated employer and skill classes to store in sql

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -18,7 +18,7 @@ public abstract class AbstractEntity {
     private int id;
 
     @NotBlank(message="Name is required")
-    @Size(min=3, max=100, message="Name must be between 3 and 50 characters")
+    @Size(min=3, max=100, message="Name must be between 3 and 100 characters")
     private String name;
 
     public int getId() {
